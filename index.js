70ceea4cfa16d88a627d00ff017985ceb40bdf06
Roberto <mirandaroberto1994@gmail.com>
2023-04-07 22:32:57 -0300

    conecta a db

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import conectarDB from "./config/db.js";
 
 const app = express();
 
-app.listen(4000, () => {
-    console.log("Server is running on port 4000")
+dotenv.config();
+
+conectarDB();
+
+const PORT = process.env.PORT || 4000;
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 }) 
