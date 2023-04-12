3ab1b120942af30e7ae6860e8642c5bd4e96ca4f
Roberto <mirandaroberto1994@gmail.com>
2023-04-12 02:02:56 -0300

    crea Routing de Usuarios

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import conectarDB from "./config/db.js";
+import usuariosRoutes from './routes/usuarioRoutes.js'
 
 const app = express();
 
 dotenv.config();
 
 conectarDB();
+
+// Routing
+app.use("/api/usuarios", usuariosRoutes);
 
 const PORT = process.env.PORT || 4000;
 
