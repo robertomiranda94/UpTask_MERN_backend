f25543b830c840485d4d4ffe93a07aa2cd45945e
Roberto <mirandaroberto1994@gmail.com>
2023-04-18 01:40:51 -0300

    configura variables de entorno y cors

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -3,133 +3,135 @@ import generarJWT from "../helpers/generarJWT.js";
 import Usuario from "../models/Usuario.js";
 
 const registrar = async (req, res) => {
-  // Evitar usuarios duplicados
-  const { email } = req.body;
-  const existeUsuario = await Usuario.findOne({ email });
-  if (existeUsuario) {
-    const error = new Error("Usuario ya registrado");
-    return res.status(400).json({ msg: error.message });
-  }
-  try {
-    const usuario = new Usuario(req.body);
-    usuario.token = generarId();
-    const usuarioAlmacenado = await usuario.save();
-    res.json(usuarioAlmacenado);
-  } catch (error) {
-    console.log(error);
-  }
+    // Evitar usuarios duplicados
+    const { email } = req.body;
+    const existeUsuario = await Usuario.findOne({ email });
+    if (existeUsuario) {
+        const error = new Error("Usuario ya registrado");
+        return res.status(400).json({ msg: error.message });
+    }
+    try {
+        const usuario = new Usuario(req.body);
+        usuario.token = generarId();
+        await usuario.save();
+        res.json({
+            msg: "Usuario registrado correctamente, Revisa tu email para confirmar tu cuenta",
+        });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 const autenticar = async (req, res) => {
-  const { email, password } = req.body;
-  // Comprobar si el usuario existe
-  const usuario = await Usuario.findOne({ email });
-  if (!usuario) {
-    const error = new Error("El Usuario no existe");
-    return res.status(404).json({ msg: error.message });
-  }
+    const { email, password } = req.body;
+    // Comprobar si el usuario existe
+    const usuario = await Usuario.findOne({ email });
+    if (!usuario) {
+        const error = new Error("El Usuario no existe");
+        return res.status(404).json({ msg: error.message });
+    }
 
-  // Comprobar si el usuario esta confirmado
-  if (!usuario.confirmado) {
-    const error = new Error("Tu cuenta no ha sido confirmada");
-    return res.status(403).json({ msg: error.message });
-  }
+    // Comprobar si el usuario esta confirmado
+    if (!usuario.confirmado) {
+        const error = new Error("Tu cuenta no ha sido confirmada");
+        return res.status(403).json({ msg: error.message });
+    }
 
-  // Comprobar si el password es correcto
-  if (await usuario.comprobarPassword(password)) {
-    res.json({
-      _id: usuario._id,
-      nombre: usuario.nombre,
-      email: usuario.email,
-      token: generarJWT(usuario._id),
-    });
-  } else {
-    const error = new Error("El password es incorrecto");
-    return res.status(403).json({ msg: error.message });
-  }
+    // Comprobar si el password es correcto
+    if (await usuario.comprobarPassword(password)) {
+        res.json({
+            _id: usuario._id,
+            nombre: usuario.nombre,
+            email: usuario.email,
+            token: generarJWT(usuario._id),
+        });
+    } else {
+        const error = new Error("El password es incorrecto");
+        return res.status(403).json({ msg: error.message });
+    }
 };
 
 const confirmar = async (req, res) => {
-  const { token } = req.params;
-  const usuarioConfirmar = await Usuario.findOne({ token });
-  if (!usuarioConfirmar) {
-    const error = new Error("Token no válido");
-    return res.status(403).json({ msg: error.message });
-  }
+    const { token } = req.params;
+    const usuarioConfirmar = await Usuario.findOne({ token });
+    if (!usuarioConfirmar) {
+        const error = new Error("Token no válido");
+        return res.status(403).json({ msg: error.message });
+    }
 
-  try {
-    usuarioConfirmar.confirmado = true;
-    usuarioConfirmar.token = "";
-    await usuarioConfirmar.save();
-    res.json({ msg: "Usuario confirmado correctamente" });
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        usuarioConfirmar.confirmado = true;
+        usuarioConfirmar.token = "";
+        await usuarioConfirmar.save();
+        res.json({ msg: "Usuario confirmado correctamente" });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 const olvidePassword = async (req, res) => {
-  const { email } = req.body;
-  const usuario = await Usuario.findOne({ email });
-  if (!usuario) {
-    const error = new Error("El Usuario no existe");
-    return res.status(404).json({ msg: error.message });
-  }
+    const { email } = req.body;
+    const usuario = await Usuario.findOne({ email });
+    if (!usuario) {
+        const error = new Error("El Usuario no existe");
+        return res.status(404).json({ msg: error.message });
+    }
 
-  try {
-    usuario.token = generarId();
-    await usuario.save();
-    res.json({ msg: "Hemos enviado un mail con las intrucciones" });
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        usuario.token = generarId();
+        await usuario.save();
+        res.json({ msg: "Hemos enviado un mail con las intrucciones" });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 const comprobarToken = async (req, res) => {
-  const { token } = req.params;
+    const { token } = req.params;
 
-  const tokenValido = await Usuario.findOne({ token });
+    const tokenValido = await Usuario.findOne({ token });
 
-  if (tokenValido) {
-    res.json({ msg: "Token válido y el usuario existe" });
-  } else {
-    const error = new Error("Token no válido");
-    return res.status(404).json({ msg: error.message });
-  }
+    if (tokenValido) {
+        res.json({ msg: "Token válido y el usuario existe" });
+    } else {
+        const error = new Error("Token no válido");
+        return res.status(404).json({ msg: error.message });
+    }
 };
 
 const nuevoPassword = async (req, res) => {
-  const { token } = req.params;
-  const { password } = req.body;
+    const { token } = req.params;
+    const { password } = req.body;
 
-  const usuario = await Usuario.findOne({ token });
+    const usuario = await Usuario.findOne({ token });
 
-  if (usuario) {
-    usuario.password = password;
-    usuario.token = "";
-    try {
-      await usuario.save();
-      res.json({ msg: "Contraseña actualizada correctamente" });
-    } catch (error) {
-      console.log(error);
+    if (usuario) {
+        usuario.password = password;
+        usuario.token = "";
+        try {
+            await usuario.save();
+            res.json({ msg: "Contraseña actualizada correctamente" });
+        } catch (error) {
+            console.log(error);
+        }
+    } else {
+        const error = new Error("Token no válido");
+        return res.status(404).json({ msg: error.message });
     }
-  } else {
-    const error = new Error("Token no válido");
-    return res.status(404).json({ msg: error.message });
-  }
 };
 
 const perfil = async (req, res) => {
-  const { usuario } = req;
+    const { usuario } = req;
 
-  res.json(usuario);
+    res.json(usuario);
 };
 
 export {
-  registrar,
-  autenticar,
-  confirmar,
-  olvidePassword,
-  comprobarToken,
-  nuevoPassword,
-  perfil,
+    registrar,
+    autenticar,
+    confirmar,
+    olvidePassword,
+    comprobarToken,
+    nuevoPassword,
+    perfil,
 };
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import conectarDB from "./config/db.js";
-import usuarioRoutes from './routes/usuarioRoutes.js'
-import proyectoRoutes from './routes/proyectoRoutes.js'
-import tareaRoutes from './routes/tareaRoutes.js'
+import usuarioRoutes from "./routes/usuarioRoutes.js";
+import proyectoRoutes from "./routes/proyectoRoutes.js";
+import tareaRoutes from "./routes/tareaRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,23 @@ dotenv.config();
 
 conectarDB();
 
+// Configurar CORS
+const whitelist = [process.env.FRONTEND_URL];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.includes(origin)) {
+            // Puede consultar la API
+            callback(null, true);
+        } else {
+            // No puede consultar la API
+            callback(new Error("No permitido por CORS"));
+        }
+    },
+};
+
+app.use(cors(corsOptions));
+
 // Routing
 app.use("/api/usuarios", usuarioRoutes);
 app.use("/api/proyectos", proyectoRoutes);
@@ -19,7 +37,6 @@ app.use("/api/tareas", tareaRoutes);
 
 const PORT = process.env.PORT || 4000;
 
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-}) 
+    console.log(`Server is running on port ${PORT}`);
+});
