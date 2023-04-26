7bec9442c6d999c2be1ffdb73fed9c335ddca62c
Roberto <mirandaroberto1994@gmail.com>
2023-04-26 01:44:54 -0300

    almacena id de tareas en Proyectos

--- models/Proyecto.js
+++ models/Proyecto.js
@@ -1,35 +1,43 @@
 import mongoose from "mongoose";
 
 const ProyectosSchema = new mongoose.Schema(
-  {
-    nombre: {
-      type: String,
-      trim: true,
-      required: true,
+    {
+        nombre: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        descripcion: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        fechaEntrega: {
+            type: Date,
+            default: Date.now(),
+        },
+        cliente: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        creador: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Usuario",
+        },
+        tareas: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Tarea",
+            },
+        ],
+        colaboradores: [
+            { type: mongoose.Schema.Types.ObjectId, ref: "Usuario" },
+        ],
     },
-    descripcion: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    fechaEntrega: {
-      type: Date,
-      default: Date.now(),
-    },
-    cliente: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    creador: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Usuario",
-    },
-    colaboradores: [{ type: mongoose.Schema.Types.ObjectId, ref: "Usuario" }],
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 const Proyecto = mongoose.model("Proyecto", ProyectosSchema);
