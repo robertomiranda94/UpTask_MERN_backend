53efcde2d53d87980d27c8dbe85568317e776587
Roberto <mirandaroberto1994@gmail.com>
2023-04-27 01:53:32 -0300

    formatea obtenerProyectos

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -2,7 +2,10 @@ import Proyecto from "../models/Proyecto.js";
 import Tarea from "../models/Tarea.js";
 
 const obtenerProyectos = async (req, res) => {
-    const proyectos = await Proyecto.find().where("creador").equals(req.usuario);
+    const proyectos = await Proyecto.find()
+        .where("creador")
+        .equals(req.usuario)
+        .select("-tareas");
     res.json(proyectos);
 };
 
@@ -21,7 +24,7 @@ const nuevoProyecto = async (req, res) => {
 const obtenerProyecto = async (req, res) => {
     const { id } = req.params;
 
-    const proyecto = await Proyecto.findById(id);
+    const proyecto = await Proyecto.findById(id).populate("tareas");
 
     if (!proyecto) {
         const error = new Error("Proyecto no encontrado");
@@ -33,8 +36,7 @@ const obtenerProyecto = async (req, res) => {
         return res.status(401).json({ msg: error.message });
     }
 
-    
-    res.json(proyecto)
+    res.json(proyecto);
 };
 
 const editarProyecto = async (req, res) => {
@@ -91,7 +93,6 @@ const eliminarProyecto = async (req, res) => {
 const agregarColaborador = async (req, res) => {};
 
 const eliminarColaborador = async (req, res) => {};
-
 
 export {
     obtenerProyectos,
